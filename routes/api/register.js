527630d707f39f6cdca5fa0d3e576b13d65d004e
William Law <horizon.william14@gmail.com>
2020-01-18 12:11:26 -0500

    reeeeeeeeeeee

--- routes/api/register.js
+++ routes/api/register.js
@@ -12,20 +12,42 @@ router.post("/", (req, res) => {
 
   //console.log(req.body);
 
-  console.log(req.body.message);
+  //console.log(req.body.message);
   //return req.body["message"];
   //return res.json(req.body);
 
-  const exec = require('child_process').exec, child;
-  var counter = 0;
-  const myShellScript = exec(`git add . && git commit -m 'init${counter == 0 ? counter++ : counter++}'`);
-  myShellScript.stdout.on('data', (data)=>{
-      console.log(data); 
-      // do whatever you want here with data
+  const { spawn } = require("child_process");
+  //var counter = 0;
+  spawn(
+    //cd ../../justcollab-site && touch 2abc.html && echo "${req.body.message}" >> 2abc.html
+    `touch 2abc.html`,
+    {
+      detached: true
+    }
+  );
+
+  /* const ss = spawn(
+    `git add . && git commit -m 'init${
+      counter == 0 ? counter++ : counter++
+    }' && git push -u origin master`,
+    { detached: true }
+  ); */
+
+  myShellScript.stdout.on("data", data => {
+    console.log(data);
+    // do whatever you want here with data
   });
-  myShellScript.stderr.on('data', (data)=>{
-      console.error(data);
+  myShellScript.stderr.on("data", data => {
+    console.error(data);
   });
+
+  /* ss.stdout.on("data", data => {
+    console.log(data);
+    // do whatever you want here with data
+  });
+  ss.stderr.on("data", data => {
+    console.error(data);
+  }); */
 
   return res.json("{'url': 'justcollab'}");
 });
